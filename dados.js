bd16b62657bf147ab0eb36eaba687a776d077cbb
Gleiciane Sousa <devgleiciane@gmail.com>
2024-09-07 20:04:17 -0300

    Salvo

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [ 
+    {
+        titulo : "Enzo",
+        descricao : "O amor do primeiro filho é um vínculo único e especial que transforma a vida dos pais, marcado por uma intensa conexão emocional e um aprendizado contínuo.",
+        link : "https://www.instagram.com/p/C9d4fqIuz1y/",
+        tags : " Alegria, amor, paz, força, coragem, esperança, gentileza, curiosidade, Brincalhão, criativo, inteligente, aventureiro, sonhador, carinhoso, "
+    },
+    {
+        titulo : "Matheus",
+        descricao : "O amor de um segundo filho traz uma nova dinâmica para a família, fortalecendo os laços e proporcionando novas experiências.",
+        link : "https://www.instagram.com/p/C9dunTypewK/",
+        tags : " Alegria, amor, paz, força, coragem, esperança, gentileza, curiosidade, Brincalhão, criativo, inteligente, aventureiro, sonhador, carinhoso, "
+    },
+    {
+        titulo : "Miguel",
+        descricao : "O terceiro filho completa a família e traz uma alegria única, tornando a vida ainda mais especial, cheia de desafios e com muito mais amor.",
+        link : "https://www.instagram.com/p/C9d6yt6uICs/",
+        tags : " Alegria, amor, paz, força, coragem, esperança, gentileza, curiosidade, Brincalhão, criativo, inteligente, aventureiro, sonhador, carinhoso, "
+    }
+];
+
+
